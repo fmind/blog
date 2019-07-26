2e09afdee8e707a60c2f72eeaa812a6a05822bd6
Médéric Hurier (fmind) <fmind@fmind.me>
2019-07-26 18:20:52 +0200

    switched to invoke

---
+++ tasks.py
@@ -0,0 +1,35 @@
+from invoke import task
+from pathlib import Path
+
+
+@task
+def venv(c, force=False):
+    """Create a virtual environment."""
+    if Path("venv").exists() and not force:
+        return None
+    c.run("python3 -m venv venv --clear")
+    c.run("venv/bin/pip install --requirement requirements.txt")
+
+
+@task(venv)
+def style(c):
+    """Create the code style for the site."""
+    c.run('pygmentize -f html -a .highlight -S colorful > theme/fmind/static/styles/pygment.css')
+
+
+@task(venv)
+def serve(c):
+    """Serve and auto-regenerate the website."""
+    c.run('venv/bin/pelican --verbose --listen --autoreload --relative-urls --output local content')
+
+
+@task(venv)
+def build(c):
+    """Generate the remote content of the site."""
+    c.run('venv/bin/pelican --verbose --ignore-cache --delete-output-directory --output output content')
+
+
+@task(build)
+def deploy(c):
+    """Deploy the content on the remote master branch."""
+    c.run('git add output; git commit -m "$(shell date +%s)"; git subtree push --prefix output origin master')
